2ab570cc5c060b54de4b6a64b2c03fa6815ddb52
Siddharth Mishra <siddharth.mishra@motorolasolutions.com>
2016-10-21 12:21:13 -0600

    Implemented Form data stuff.

--- Shaping-up-with-AngularJs/app.js
+++ Shaping-up-with-AngularJs/app.js
@@ -277,4 +277,12 @@
         };
     });
 
+    app.controller('ReviewController', function(){
+    this.review={};
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
 })();
