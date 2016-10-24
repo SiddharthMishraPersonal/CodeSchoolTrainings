7b39291fe74ba74dc9d152a92d692959b7f97c3c
Siddharth Mishra <siddharth.mishra@motorolasolutions.com>
2016-10-24 09:57:33 -0600

    Created controller for Product-Panels directive.

--- Shaping-up-with-AngularJs/app.js
+++ Shaping-up-with-AngularJs/app.js
@@ -324,4 +324,21 @@
 
         };
     });
+
+    app.directive('productPanels', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'directives/product-panels/index.html',
+            controller: function () {
+                this.tab = 1;
+                this.selectTab = function (setTab) {
+                    this.tab = setTab;
+                };
+                this.isSelected = function (checkTab) {
+                    return this.tab === checkTab;
+                };
+            },
+            controllerAs: 'panel'
+        };
+    });
 })();
